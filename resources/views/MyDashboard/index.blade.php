49c1c35b045a068e32ac3161da3cdbf8a8086e94
muhammadaulia <17523058@alumni.uii.ac.id>
2022-05-19 14:07:24 +0700

    commit

--- resources/views/MyDashboard/index.blade.php
+++ resources/views/MyDashboard/index.blade.php
@@ -1,10 +1,58 @@
 @extends('mainLayout.main')
 
-@section('content')
+{{-- CSS --}}
+<link rel="stylesheet" href="css/MyDashBoard/style.css">
 
-  <div>
+@section('content')
+  
+  <main>
+    
+    <div class="d-flex flex-column flex-shrink-0 p-3 bg-light" style="width: 280px;">
+      <a href="/" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <svg class="bi me-2" width="40" height="32"><use xlink:href="#people-circle"/></svg>
+        <span class="fs-4">{{ auth()->user()->name }}</span>
+      </a>
+      <hr>
+      <ul class="nav nav-pills flex-column mb-auto">
+        <li class="nav-item">
+          <a href="#" class="nav-link active" aria-current="page">
+            <svg class="bi me-2" width="16" height="16"><use xlink:href="#home"/></svg>
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link link-dark">
+            <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
+            Dashboard
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link link-dark">
+            <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+            Orders
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link link-dark">
+            <svg class="bi me-2" width="16" height="16"><use xlink:href="#grid"/></svg>
+            Products
+          </a>
+        </li>
+        <li>
+          <a href="#" class="nav-link link-dark">
+            <svg class="bi me-2" width="16" height="16"><use xlink:href="#people-circle"/></svg>
+            Customers
+          </a>
+        </li>
+      </ul>
+    </div>
+      
+  </main>
+
+
+  {{-- <div>
     <h1>Welcome to your own dashboard page, you are <u>{{ auth()->user()->name }}</u> 
     </h1>
-  </div>
+  </div> --}}
 
 @endsection
