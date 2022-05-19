22c4f9c9ee2e009b844f36449c04238be55f296b
muhammadaulia <17523058@alumni.uii.ac.id>
2022-05-19 20:02:11 +0700

    commit

--- resources/views/MyDashboard/index.blade.php
+++ resources/views/MyDashboard/index.blade.php
@@ -84,7 +84,7 @@
       </nav>
   
       <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3">
           
           <div class="row">
             <div>
@@ -93,7 +93,69 @@
                   </button>
             </div>
 
-            <h1 class="h2 mt-3">Dashboard</h1>
+            <h1 class="h2 mt-3 pb-2 border-bottom">Dashboard</h1>
+
+            <div>
+              <canvas class="my-5 w-100" id="myChart" width="900" height="380">
+                <script>
+                  const labels = [
+                    'Januari',
+                    'Februari',
+                    'Maret',
+                    'April',
+                    'Mei',
+                    'Juni',
+                    'Juli',
+                    'Agustus',
+                    'September',
+                    'Oktober',
+                    'November',
+                    'Desember',
+                  ];
+                
+                  const data = {
+                    labels: labels,
+                    datasets: [{ // index pertama / datasets[0]
+                      label: 'Jumlah Jimpitan',
+                      backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(255, 159, 64, 0.2)',
+                      'rgba(255, 205, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(201, 203, 207, 0.2)'
+                    ],
+                    borderColor: [
+                      'rgb(255, 99, 132)',
+                      'rgb(255, 159, 64)',
+                      'rgb(255, 205, 86)',
+                      'rgb(75, 192, 192)',
+                      'rgb(54, 162, 235)',
+                      'rgb(153, 102, 255)',
+                      'rgb(201, 203, 207)'
+                    ],
+                      data: [1000000, 2000000, 5000000, 2000000, 2500000, 3500000, 4500000],
+                      showLine: true,
+                      spanGaps: true,
+                    }]
+                  };
+                
+                  const config = {
+                    type: 'bar',
+                    data: data,
+                    options: {}
+                  };
+                
+                  const myChart = new Chart(
+                    document.getElementById('myChart'),
+                    config
+                  );
+      
+                </script>
+              </canvas>
+            </div>
+      
 
           </div>
           
